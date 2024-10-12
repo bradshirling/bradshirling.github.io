0d274e85f86368042301a48ed12d362206d893fa
bradshirling <bradleyshirling@gmail.com>
2024-10-12 16:01:32 -0500

    first functional portfolio @ bradleyshirling.io

---
+++ js/sortVisualizerScript.js
@@ -0,0 +1,113 @@
+const array=[];
+
+let audioCtx=null;
+
+let sorting = false;
+
+function getInputValues(){
+    const n =parseInt(document.getElementById("numOfDigits").value);
+    const s =parseInt(document.getElementById("speed").value);
+    return {n,s};
+}
+
+function playNote(freq){
+    if (audioCtx==null){
+        audioCtx=new(
+            AudioContext ||
+            webkitAudioContext ||
+            window.webkitAudioContext
+        )();
+    }
+    const dur=0.1;
+    const osc=audioCtx.createOscillator();
+    osc.frequency.value=freq;
+    osc.start();
+    osc.stop(audioCtx.currentTime+dur);
+    const node=audioCtx.createGain();
+    node.gain.value=0.1;
+    node.gain.linearRampToValueAtTime(0,audioCtx.currentTime+dur);
+    osc.connect(node);
+    node.connect(audioCtx.destination);
+}
+
+function init(){
+    if (sorting) {
+        sorting = false;
+    }
+    document.querySelector('button[onclick="play()"]').disabled = false;
+    const { n } = getInputValues();
+    array.length = 0;
+    container.innerHTML = "";
+    for(let i=0;i<n;i++){
+        array[i]=Math.random();
+    }
+    showBars();
+}
+
+function play(){
+    let { s } = getInputValues();
+    if (!s || s == 0) {
+        s = 50;
+    }
+    document.querySelector('button[onclick="play()"]').disabled = true;
+    sorting = true;
+    const copy=[...array];
+    const moves=bubbleSort(copy);
+    document.getElementById("form").reset();
+    animate(moves,s);
+}
+
+function animate(moves, s){
+    if (!sorting) return;
+    if(moves.length==0){
+        showBars();
+        document.querySelector('button[onclick="play()"]').disabled = false;
+        sorting = false;
+        return;
+    }
+    const move=moves.shift();
+    const [i,j]=move.indices;
+
+    if(move.type=="swap"){
+        [array[i],array[j]]=[array[j],array[i]];
+    }
+
+    playNote(200+array[i]*500);
+    playNote(200+array[j]*500);
+
+    showBars(move);
+    setTimeout(function(){
+        animate(moves,s);
+    },s);
+}
+
+function bubbleSort(array){
+    const moves=[];
+    do{
+        var swapped=false;
+        for(let i=1;i<array.length;i++){
+            moves.push({indices:[i-1,i],type:"comp"});
+            if(array[i-1]>array[i]){
+                swapped=true;
+                moves.push({indices:[i-1,i],type:"swap"});
+                [array[i-1],array[i]]=[array[i],array[i-1]];
+            }
+        }
+    }while(swapped);
+    return moves;
+}
+
+function showBars(move){
+    container.innerHTML="";
+    for(let i=0;i<array.length;i++){
+        const bar=document.createElement("div");
+        bar.style.height=array[i]*100+"%";
+        bar.classList.add("bar");
+
+        if(move && move.indices.includes(i)){
+            bar.style.backgroundColor=
+            move.type=="swap"?"red":"blue";
+        }
+        container.appendChild(bar);
+    }
+}
